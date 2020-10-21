643e1fda2d97aafdb7cdc0ba6eb7a5560fba5af5
Kay <kay.kasemir@gmail.com>
2020-10-21 17:06:16 -0400

    If one PV has NaN as a value, must not close the socket ...
    ... and thus lose all PVs.
    On reconnect, a 'double' PV might then again have NaN as its actual
    value, and we enter a never ending connection cycle

--- WebContent/js/pvws.js
+++ WebContent/js/pvws.js
@@ -78,17 +78,11 @@ class PVWS
             if (value === undefined)
                 value = { pv: jm.pv, readonly: true };
             
-            //Only way found to show PV as disconnected
-            if(jm.value =="NaN")
-                this.close();
-            else
-            {
-                // Update cached value with received changes
-                Object.assign(value, jm);
-                this.values[jm.pv] = value;
-                // console.log("Update for PV " + jm.pv + ": " + JSON.stringify(value));
-                this.message_handler(value);
-            }
+            // Update cached value with received changes
+            Object.assign(value, jm);
+            this.values[jm.pv] = value;
+            // console.log("Update for PV " + jm.pv + ": " + JSON.stringify(value));
+            this.message_handler(value);
         }
         else
             this.message_handler(jm);
