3217dcae0b275a7ee7a2b27d436748d1a104fa80
kasemir <ky9@ornl.gov>
2026-03-09 15:19:17 -0400

    Use 5.0.2, fix persisted PVA settings

--- src/main/java/pvws/PVWebSocketContext.java
+++ src/main/java/pvws/PVWebSocketContext.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2019-2022 UT-Battelle, LLC.
+ * Copyright (c) 2019-2026 UT-Battelle, LLC.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the LICENSE
  * which accompanies this distribution
@@ -53,12 +53,12 @@ public class PVWebSocketContext implements ServletContextListener
 
         logger.log(Level.INFO, "===========================================");
         logger.log(Level.INFO, context.getContextPath() + " started");
-   
+
         // Set default type in preferences before PVPool reads the preferences
         String default_type = System.getenv("PV_DEFAULT_TYPE");
         if (default_type != null  &&  !default_type.isEmpty())
             Preferences.userRoot().node("/org/phoebus/pv").put("default", default_type);
-            
+
         logger.log(Level.INFO, "Supported PV types: " + PVPool.getSupportedPrefixes());
 
         // Configure JCA/CAJ to use environment vars, not java properties or preferences
@@ -76,6 +76,43 @@ public class PVWebSocketContext implements ServletContextListener
                                "EPICS_PVA_AUTO_ADDR_LIST"
                             })
             logger.log(Level.INFO, name + " = " + System.getenv(name));
+
+        try
+        {
+            // We take settings from environment variables.
+            // Based on version, core-pva's PVA_Preferences might read from the default
+            // Preferences implementation,
+            // for example the Mac prefs when on Mac which persist in ~/Library/preferences/xxxx.plist.
+            // We would then use the last settings written by some phoebus tool.
+            // --> Delete those!
+            Class<?> cls = Class.forName("org.phoebus.pv.pva.PVA_Preferences");
+            Preferences prefs = Preferences.userNodeForPackage(cls);
+            for (String setting : new String[]
+                                  {
+                                    "epics_pva_addr_list",
+                                    "epics_pva_auto_addr_list",
+                                    "epics_pva_name_servers",
+                                    "epics_pva_server_port",
+                                    "epics_pva_broadcast_port",
+                                    "epics_pva_conn_tmo",
+                                    "epics_pva_tcp_socket_tmo",
+                                    "epics_pva_max_array_formatting",
+                                    "epics_pva_send_buffer_size"
+                                })
+            {
+                String value = prefs.get(setting, null);
+                if (value != null  &&  !value.isEmpty())
+                {
+                    logger.log(Level.INFO, "Clearing  " + setting + " = " + value + " from PVA_Preferences");
+                    prefs.remove(setting);
+                }
+            }
+        }
+        catch (ClassNotFoundException ex)
+        {
+            logger.log(Level.INFO, "PVA_Preferences class not found, not clearing persisted settings");
+        }
+
         logger.log(Level.INFO, "===========================================");
 
         start_time = Instant.now();
